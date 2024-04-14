9cbd90bd3567ed3325cc8f1a1a032fa5b4ae8b90
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 00:50:19 -0500

    feat: bands header

--- resources/js/Pages/Bands/Index.vue
+++ resources/js/Pages/Bands/Index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import BandsHeader from '@/Pages/Bands/Partials/BandsHeader.vue';
 </script>
 
 <template>
   <AuthenticatedLayout title="Bands">
-    <div>
-      <h1>Bands</h1>
+    <div class="max-w-4xl mx-auto py-12">
+      <BandsHeader />
     </div>
   </AuthenticatedLayout>
 </template>
---
+++ resources/js/Pages/Bands/Partials/BandsHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import InviteIcon from '@/Components/Icons/InviteIcon.vue';
+import AddIcon from '@/Components/Icons/AddIcon.vue';
+</script>
+
+<template>
+  <div class="flex justify-between items-center">
+    <h3 class="text-orange-600 text-4xl font-bold">Your Bands</h3>
+    <div class="flex gap-8">
+      <a href="#" class="flex items-center gap-4 text-3xl text-orange-600">
+        <InviteIcon />
+      </a>
+      <a href="#" class="flex items-center gap-4 text-3xl text-orange-600">
+        <AddIcon />
+      </a>
+    </div>
+  </div>
+</template>
