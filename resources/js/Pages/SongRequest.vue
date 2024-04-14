d9d51bdc9dcbfae535f6be20b4fa375c99999c37
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 15:52:58 -0500

    feat: setup song request page

--- resources/js/Pages/SongRequest.vue
+++ resources/js/Pages/SongRequest.vue
@@ -8,9 +8,24 @@ defineProps({
 });
 
 const form = useForm({
+  selectedSessions: [],
   song_name: '',
   song_artist: '',
 });
+
+const sessionIsSelected = (sessionId) => {
+  return form.selectedSessions.includes(sessionId);
+};
+
+const toggleSession = (sessionId) => {
+  if (sessionIsSelected(sessionId)) {
+    form.selectedSessions = form.selectedSessions.filter(
+      (session) => session !== sessionId
+    );
+  } else {
+    form.selectedSessions = [...form.selectedSessions, sessionId];
+  }
+};
 
 const submit = () => {
   alert('Request a song');
@@ -35,8 +50,30 @@ const submit = () => {
         <div
           class="col-span-1 max-h-[450px] overflow-y-auto border-2 border-orange-700 rounded-2xl p-4"
         >
-          <div class="flex justify-between items-center mb-4">
-            <h5 class="text-xl font-bold">Select Live Sessions</h5>
+          <h5 class="text-xl font-bold">Select Live Sessions</h5>
+          <div class="flex flex-col gap-2 mt-4">
+            <button
+              v-for="(session, index) in liveSessions"
+              :key="index"
+              class="flex items-center border-2 border-orange-400 gap-2 py-2 px-2 rounded-xl cursor-pointer hover:bg-orange-400/50 transition-all duration-300 ease-in-out"
+              :class="{
+                'bg-orange-400/50': sessionIsSelected(session.id),
+              }"
+              @click.prevent="toggleSession(session.id)"
+            >
+              <span
+                class="w-4 h-4 border-2 rounded-full transition-all duration-300 ease-in-out"
+                :class="{
+                  'border-black text-black': !sessionIsSelected(session.id),
+                  'border-orange-700 bg-orange-700': sessionIsSelected(
+                    session.id
+                  ),
+                }"
+              />
+              <span class="truncate max-w-full text-left">
+                {{ session.title }}
+              </span>
+            </button>
           </div>
         </div>
 
