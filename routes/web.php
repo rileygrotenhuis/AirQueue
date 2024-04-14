d184e3ad75bfb3dd8967066945ccf54c803edeb9
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 03:54:05 -0500

    organize routes

--- routes/web.php
+++ routes/web.php
@@ -6,36 +6,34 @@ use App\Http\Controllers\BandInvitationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware('auth:web')->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Index');
-    })->name('home');
+Route::middleware('auth:web')->get('/', function () {
+    return Inertia::render('Index');
+})->name('home');
 
-    Route::prefix('bands')->group(function () {
-        Route::get('/', [BandController::class, 'index'])->name('bands.index');
-        Route::post('/', [BandController::class, 'store'])->name('bands.store');
-        Route::get('/create', [BandController::class, 'create'])->name('bands.create');
+Route::middleware('auth:web')->prefix('bands')->group(function () {
+    Route::get('/', [BandController::class, 'index'])->name('bands.index');
+    Route::post('/', [BandController::class, 'store'])->name('bands.store');
+    Route::get('/create', [BandController::class, 'create'])->name('bands.create');
 
-        Route::get('/invitations', [BandInvitationController::class, 'index'])->name('bands.invitations');
-        Route::post('/invitations/{bandUser}/accept', [BandInvitationController::class, 'accept'])->name('bands.invitations.accept');
-        Route::post('/invitations/{bandUser}/reject', [BandInvitationController::class, 'reject'])->name('bands.invitations.reject');
-        Route::post('/{band}/invite', [BandInvitationController::class, 'invite'])->name('bands.invite');
+    Route::get('/invitations', [BandInvitationController::class, 'index'])->name('bands.invitations');
+    Route::post('/invitations/{bandUser}/accept', [BandInvitationController::class, 'accept'])->name('bands.invitations.accept');
+    Route::post('/invitations/{bandUser}/reject', [BandInvitationController::class, 'reject'])->name('bands.invitations.reject');
+    Route::post('/{band}/invite', [BandInvitationController::class, 'invite'])->name('bands.invite');
 
-        Route::get('/{band}', [BandController::class, 'show'])->name('bands.show');
-        Route::put('/{band}', [BandController::class, 'update'])->name('bands.update');
+    Route::get('/{band}', [BandController::class, 'show'])->name('bands.show');
+    Route::put('/{band}', [BandController::class, 'update'])->name('bands.update');
 
-        Route::delete('/{band}/members/{bandUser}/remove', [BandController::class, 'removeMember'])->name('bands.members.remove');
-    });
+    Route::delete('/{band}/members/{bandUser}/remove', [BandController::class, 'removeMember'])->name('bands.members.remove');
 });
 
+Route::post('/register', [AuthController::class, 'register'])->name('register-user');
 Route::get('/register', function () {
     return Inertia::render('Register');
 })->name('register');
 
+Route::post('/login', [AuthController::class, 'login'])->name('login-user');
 Route::get('/login', function () {
     return Inertia::render('Login');
 })->name('login');
 
-Route::post('/register', [AuthController::class, 'register'])->name('register-user');
-Route::post('/login', [AuthController::class, 'login'])->name('login-user');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
