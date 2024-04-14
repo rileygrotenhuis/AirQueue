a3437081161cd0f3eb6e9e8a2452a8b6c060bda7
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 01:22:47 -0500

    fix: routing

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ Route::middleware('auth:web')->group(function () {
         Route::post('/', [BandController::class, 'store'])->name('bands.store');
         Route::get('/create', [BandController::class, 'create'])->name('bands.create');
 
-        Route::get('/{band}', [BandController::class, 'show'])->name('bands.show');
-        Route::put('/{band}', [BandController::class, 'update'])->name('bands.update');
-        Route::get('/{band}/settings', [BandController::class, 'settings'])->name('bands.settings');
-
         Route::get('/invitations', [BandInvitationController::class, 'index'])->name('bands.invitations');
         Route::post('/invitations/{bandUser}/accept', [BandInvitationController::class, 'accept'])->name('bands.invitations.accept');
         Route::post('/invitations/{bandUser}/reject', [BandInvitationController::class, 'reject'])->name('bands.invitations.reject');
         Route::post('/{band}/invite/{user}', [BandInvitationController::class, 'invite'])->name('bands.invite');
+
+        Route::get('/{band}', [BandController::class, 'show'])->name('bands.show');
+        Route::put('/{band}', [BandController::class, 'update'])->name('bands.update');
+        Route::get('/{band}/settings', [BandController::class, 'settings'])->name('bands.settings');
     });
 });
 
