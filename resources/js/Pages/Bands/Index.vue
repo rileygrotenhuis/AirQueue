ed9650b863c8947c8ca5b400ad73e973a7c9b001
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 00:54:03 -0500

    wip

--- resources/js/Pages/Bands/Index.vue
+++ resources/js/Pages/Bands/Index.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import BandsHeader from '@/Pages/Bands/Partials/BandsHeader.vue';
 </script>
 
 <template>
   <AuthenticatedLayout title="Bands">
-    <div class="max-w-4xl mx-auto py-12">
-      <BandsHeader />
-    </div>
+    <div class="max-w-4xl mx-auto py-12"></div>
   </AuthenticatedLayout>
 </template>
