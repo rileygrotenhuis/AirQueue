11088df15cb1edf3db49b657e094bb93bf919f16
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 16:02:06 -0500

    typo

--- resources/js/Layouts/Partials/SessionDropdownMenu.vue
+++ resources/js/Layouts/Partials/SessionDropdownMenu.vue
@@ -30,7 +30,7 @@ const joinSessionModalOpen = ref(false);
 
         <button
           @click="startSessionModalOpen = true"
-          class="block w-full px-4 py-2 text-gstart text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+          class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
         >
           Start session
         </button>
