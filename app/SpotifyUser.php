92b1adb7243b51d564b2233aabfdf2e4cef141dd
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 23:05:33 -0500

    fix: use song request to add to queue

--- app/SpotifyUser.php
+++ app/SpotifyUser.php
@@ -2,16 +2,17 @@
 
 namespace App;
 
+use App\Models\SongRequest;
 use GuzzleHttp\Client;
 
 trait SpotifyUser
 {
-    public function addSongToQueue(): void
+    public function addSongToQueue(SongRequest $songRequest): void
     {
         try {
             $client = new Client();
 
-            $response = $client->post('https://api.spotify.com/v1/me/player/queue?uri=spotify:track:4iV5W9uYEdYUVa79Axb7Rh', [
+            $response = $client->post('https://api.spotify.com/v1/me/player/queue?uri='.$songRequest->spotify_track_id, [
                 'headers' => [
                     'content-type' => 'application/json',
                     'Authorization' => 'Bearer '.$this->spotifyTokens->first()->access_token,
