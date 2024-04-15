422f7e94bdb4b270776b90a3e61a31426d2ac7f0
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 21:36:08 -0500

    feat: update profile section

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateProfileRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -11,5 +13,16 @@ class ProfileController extends Controller
     public function index(Request $request): Response
     {
         return Inertia::render('Profile');
+    }
+
+    public function update(UpdateProfileRequest $request): RedirectResponse
+    {
+        $request->user()->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+        ]);
+
+        return to_route('profile.index');
     }
 }
--- resources/js/Pages/Profile.vue
+++ resources/js/Pages/Profile.vue
@@ -1,11 +1,75 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { useForm, usePage } from '@inertiajs/vue3';
+import InputError from '@/Components/InputError.vue';
+
+const user = usePage().props.auth.user;
+
+const form = useForm({
+  first_name: user.first_name,
+  last_name: user.last_name,
+  email: user.email,
+});
+
+const submit = () => {
+  form.put(route('profile.update'));
+};
 </script>
 
 <template>
   <AuthenticatedLayout title="Bands">
     <div class="max-w-5xl mx-auto py-12 px-4">
-      <h1>Profile</h1>
+      <form @submit.prevent="submit" class="space-y-6">
+        <div>
+          <h3 class="text-orange-700 font-bold text-3xl mb-1">
+            Account Settings
+          </h3>
+          <p class="font-light text-base">
+            Manage your account profile details and integration settings.
+          </p>
+        </div>
+        <div>
+          <label class="text-lg mb-2" for="first_name">First Name</label>
+          <input
+            v-model="form.first_name"
+            type="text"
+            id="first_name"
+            class="px-4 py-2 w-full rounded-lg border justify-start items-center gap-2.5 inline-flex text-black placeholder-opacity-10 focus:ring-orange-300 focus:border-orange-300"
+            placeholder="Enter your first name..."
+          />
+          <InputError class="mt-2" :message="form.errors.first_name" />
+        </div>
+        <div>
+          <label class="text-lg mb-2" for="last_name">Last Name</label>
+          <input
+            v-model="form.last_name"
+            type="text"
+            id="last_name"
+            class="px-4 py-2 w-full rounded-lg border justify-start items-center gap-2.5 inline-flex text-black placeholder-opacity-10 focus:ring-orange-300 focus:border-orange-300"
+            placeholder="Enter your last name..."
+          />
+          <InputError class="mt-2" :message="form.errors.last_name" />
+        </div>
+        <div>
+          <label class="text-lg mb-2" for="email">Email</label>
+          <input
+            v-model="form.email"
+            type="email"
+            id="email"
+            class="px-4 py-2 w-full rounded-lg border justify-start items-center gap-2.5 inline-flex text-black placeholder-opacity-10 focus:ring-orange-300 focus:border-orange-300"
+            placeholder="Enter your email address..."
+          />
+          <InputError class="mt-2" :message="form.errors.email" />
+        </div>
+        <div>
+          <button
+            type="submit"
+            class="float-right w-[150px] bg-orange-700 text-white px-4 py-2 rounded-lg border border-orange-700 hover:bg-orange-300 hover:text-orange-900 font-bold hover:border-orange-300 transition-colors duration-300 ease-in-out hover:cursor-pointer"
+          >
+            Create
+          </button>
+        </div>
+      </form>
     </div>
   </AuthenticatedLayout>
 </template>
