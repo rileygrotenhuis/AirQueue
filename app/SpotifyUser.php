4a99daf0f249d1a91e06d04e55461d932ff67da9
Riley Grotenhuis <rg@buildmidwestern.com>
2024-04-14 23:06:25 -0500

    fix: remove song reuqest after adding

--- app/SpotifyUser.php
+++ app/SpotifyUser.php
@@ -12,16 +12,14 @@ trait SpotifyUser
         try {
             $client = new Client();
 
-            $response = $client->post('https://api.spotify.com/v1/me/player/queue?uri='.$songRequest->spotify_track_id, [
+            $client->post('https://api.spotify.com/v1/me/player/queue?uri='.$songRequest->spotify_track_id, [
                 'headers' => [
                     'content-type' => 'application/json',
                     'Authorization' => 'Bearer '.$this->spotifyTokens->first()->access_token,
                 ],
             ]);
 
-            if ($response->getStatusCode() !== 204) {
-                logger('Failed to add song to queue');
-            }
+            $songRequest->delete();
         } catch (\Exception $e) {
             logger($e->getMessage());
         }
